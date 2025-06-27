492e2b82ecad406a46282de8a4e6131616241670
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-06-27 11:27:23 -0300

    feat: Added selenium examples for web scrapping.

---
+++ python-libraries/aula27.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://127.0.0.1:3333/'
+response = requests. get (url)
+raw_html = response. text
+parsed_html = BeautifulSoup(raw_html, 'html.parser' )
+
+top_jobs_heading = parsed_html.select_one('#intro > div > div > article > h2')
+
+if top_jobs_heading is not None:
+    article = top_jobs_heading.parent
+
+    if article is not None:
+        for p in article.select('p'):
+            print(re.sub(r'\s{1,}', ' ', p.text).strip())
